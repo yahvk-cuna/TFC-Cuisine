638a76e8db53f7ee72d926070f60e87011ab9d6f
yahvk <71e6fd52@gmail.com>
2026-02-24 15:17:16 +0800

    Compatible with older versions of Forge, close #1

--- src/main/java/moe/yahvk/tfc_cuisine/TFCCuisine.java
+++ src/main/java/moe/yahvk/tfc_cuisine/TFCCuisine.java
@@ -2,6 +2,7 @@ package moe.yahvk.tfc_cuisine;
 
 import com.mojang.logging.LogUtils;
 import moe.yahvk.tfc_cuisine.config.Config;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
@@ -11,7 +12,12 @@ public class TFCCuisine {
     public static final String MODID = "tfc_cuisine";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public TFCCuisine(FMLJavaModLoadingContext context) {
+//    public TFCCuisine(FMLJavaModLoadingContext context) {
+//        Config.registerConfigs(context);
+//    }
+    public TFCCuisine() {
+        @SuppressWarnings("removal")  // Compatible with older versions of Forge
+        var context = ModLoadingContext.get();
         Config.registerConfigs(context);
     }
 }
